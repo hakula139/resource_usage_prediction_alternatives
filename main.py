133a10912e2d297d2d1edb4195f4ebee09f4e5ce
hakulachen <hakulachen@tencent.com>
2021-07-22 11:12:19 +0800

    feat: measure time for training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import List
 import os
-from statsmodels.tsa.arima.model import ARIMA, ARIMAResults
+from time import process_time_ns
+from statsmodels.tsa.statespace.sarimax import SARIMAX, SARIMAXResults
 from utils import read_data, plot_predictions, plot_train_loss
 
 
@@ -13,7 +14,7 @@ if __name__ == '__main__':
     ARIMA_P = 5  # autoregressive model parameter
     ARIMA_D = 1  # integrated model parameter
     ARIMA_Q = 1  # moving average model parameter
-    LOSS_THRESHOLD = 20
+    LOSS_THRESHOLD = 2
 
     DATA_DIR = 'data'
     INPUT_PATH = os.path.realpath(os.path.join(DATA_DIR, 'input.txt'))
@@ -41,16 +42,24 @@ if __name__ == '__main__':
         train_loss_x, naive_loss_x = [], []
         train_loss_y, naive_loss_y = [], []
 
+        total_time, max_time, time_count = 0.0, 0.0, 0
+
         with open(OUTPUT_PATH, 'w') as output_file:
             for i in range(BATCH_SIZE, len(dataset)):
                 epoch = i + 1
+                start_time = process_time_ns()
 
-                data = dataset[i - BATCH_SIZE:i]
-                model = ARIMA(data, order=(ARIMA_P, ARIMA_D, ARIMA_Q))
-                model_fit: ARIMAResults = model.fit(
-                    method_kwargs={"warn_convergence": False}
+                data = dataset[:i]
+                model = SARIMAX(
+                    data,
+                    order=(ARIMA_P, ARIMA_D, ARIMA_Q),
+                    initialization='approximate_diffuse',
                 )
-                train_loss: float = model_fit.mse / ARIMA_P
+                model_fit: SARIMAXResults = model.fit(
+                    disp=False,
+                    warn_convergence=False,
+                )
+                train_loss: float = model_fit.mse
                 train_loss_x.append(epoch)
                 train_loss_y.append(train_loss)
 
@@ -64,10 +73,21 @@ if __name__ == '__main__':
                 prediction_x.append(epoch + 1)
                 prediction_y.append(prediction)
 
-                print('> {} (naive: {})   \tLoss: {:10.5f} (train)'.format(
-                    prediction, naive_pred, train_loss
-                ))
+                end_time = process_time_ns()
+                time = (end_time - start_time) / 1e6
+                total_time += time
+                max_time = max(max_time, time)
+                time_count += 1
+
+                print(
+                    f'> {prediction} (naive: {naive_pred})   \t'
+                    f'Loss: {train_loss:9.5f} (train) \t'
+                    f'Time: {time:.4f} ms'
+                )
                 output_file.write(f'{prediction} ')
+
+        avg_time = total_time / time_count
+        print(f'Time: {avg_time:.4f} ms (average) | {max_time:.4f} ms (max)')
 
         plot_predictions(
             PREDICTIONS_FIGURE_PATH,
